dc9830c0dce7cd1ce023ec57d9826a6a4e03e083
Bhargavi <bhargavi.pendyala@techvedika.com>
2019-05-16 11:24:01 +0530

    updated comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,12 @@ class ApplicationController < ActionController::Base
   before_action :store_current_location, :unless => :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-
   private
 
   def store_current_location
     store_location_for(:user, request.url)
   end
 
-  # Send 'em back where they came from with a slap on the wrist
   def user_not_authorized
     redirect_to request.referrer.presence || root_path, :alert => 'You are not authorized to complete that action.'
   end
